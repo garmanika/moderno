a76badac629aa2b60d981b2e2eea622f9c2a2483
garmanika <aleks.orlov97@gmail.com>
2019-10-14 01:42:40 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,31 @@ $(function(){
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   $(".js-range-slider").ionRangeSlider({
@@ -47,7 +72,9 @@ $(function(){
   $('.header__btn-menu').on('click' , function(){
     $('.header__box').toggleClass('active');
   });
-  
+
+
+	$('input[type="file"], select').styler();
 
   var mixer = mixitup('.products__inner-box');
 
